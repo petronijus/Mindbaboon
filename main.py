44fdbfd08a859fba5bd52a5d529eec5fec8a0785
petronijus <petronijus@users.noreply.github.com>
2023-02-05 22:14:25 +0100

    initial, not working db

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask("Mindbaboon")
 
+def get_db():
+    db = sqlite3.connect('todos.db')
+    return db
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/')
+def index():
+    db = get_db()
+    cur = db.execute('SELECT id, task FROM todos')
+    todos = cur.fetchall()
+    return render_template('index.html', todos=todos)
 
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form['task']
+    db = get_db()
+    db.execute('INSERT INTO todos (task) VALUES (?)', [task])
+    db.commit()
+    return redirect(url_for('index'))
 
-# Press the green button in the gutter to run the script.
+@app.route('/edit/<int:todo_id>')
+def edit(todo_id):
+    db = get_db()
+    cur = db.execute('SELECT id, task FROM todos WHERE id = ?', [todo_id])
+    todo = cur.fetchone()
+    return render_template('edit.html', todo=todo)
+
+@app.route('/update/<int:todo_id>', methods=['POST'])
+def update(todo_id):
+    task = request.form['task']
+    db = get_db()
+    db.execute('UPDATE todos SET task = ? WHERE id = ?', [task, todo_id])
+    db.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    db = get_db()
+    db.execute('DELETE FROM todos WHERE id = ?', [todo_id])
+    db.commit()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
